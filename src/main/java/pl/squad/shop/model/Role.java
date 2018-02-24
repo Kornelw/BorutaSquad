ac68361252e85cf7528e12b7524127d4d97c5ece
adam_jirik <adam.jirik1@gmail.com>
2018-02-24 11:44:06 +0100

    sorry, no entity and import

--- src/main/java/pl/squad/shop/model/Role.java
+++ src/main/java/pl/squad/shop/model/Role.java
@@ -1,6 +1,7 @@
 package pl.squad.shop.model;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
