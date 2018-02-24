89eb43805eff6d3dc18a49ba397a8409e052d368
adam_jirik <adam.jirik1@gmail.com>
2018-02-24 11:38:22 +0100

    user and role models

---
+++ src/main/java/pl/squad/shop/model/Role.java
@@ -0,0 +1,51 @@
+package pl.squad.shop.model;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Role {
+
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	private long id;
+	
+	@Column
+	private String role;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+	public Role(long id, String role) {
+		this.id = id;
+		this.role = role;
+	}
+
+	public Role(String role) {
+		this.role = role;
+	}
+
+	
+	public Role() {
+	}
+
+	@Override
+	public String toString() {
+		return "Role [id=" + id + ", role=" + role + "]";
+	}
+
+}
---
+++ src/main/java/pl/squad/shop/model/User.java
@@ -0,0 +1,105 @@
+package pl.squad.shop.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.validator.constraints.Email;
+
+
+
+
+@Entity
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long Id;
+
+	@Column(unique=true)
+	
+	@Email
+	private String email;
+	
+	@ManyToOne
+	@JoinColumn(name="role_id")
+	private Role role;
+	
+	public User() {
+		super();
+	}
+
+	public User(String email, String name, String lastName, Role role) {
+		super();
+		this.email = email;
+		this.name = name;
+		this.lastName = lastName;
+		this.role = role;
+	}
+
+	public User(long id, String email, String name, String lastName, Role role) {
+		super();
+		Id = id;
+		this.email = email;
+		this.name = name;
+		this.lastName = lastName;
+		this.role = role;
+	}
+
+	public long getId() {
+		return Id;
+	}
+
+	public void setId(long id) {
+		Id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	@Override
+	public String toString() {
+		return "User [Id=" + Id + ", email=" + email + ", name=" + name + ", lastName=" + lastName + ", role=" + role
+				+ "]";
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
+	private String name;
+
+	private String lastName;
+	
+
+//	
+}
