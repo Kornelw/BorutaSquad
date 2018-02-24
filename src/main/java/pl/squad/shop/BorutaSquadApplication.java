8dad1a92e3c41c037a29db5fc496fb3291d3f4d9
KornelW <kornel.wojtasiak@gmail.com>
2018-02-24 10:37:47 +0100

    project

---
+++ src/main/java/pl/squad/shop/BorutaSquadApplication.java
@@ -0,0 +1,12 @@
+package pl.squad.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BorutaSquadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BorutaSquadApplication.class, args);
+	}
+}
