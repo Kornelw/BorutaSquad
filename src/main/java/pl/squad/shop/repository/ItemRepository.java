b9020998a3c446e8b90cf69dd6471217e79ca9ae
Michal_Fromlewicz <michal.fromlewicz@gmail.com>
2018-02-24 12:00:33 +0100

    ItemRepository added

---
+++ src/main/java/pl/squad/shop/repository/ItemRepository.java
@@ -0,0 +1,13 @@
+package pl.squad.shop.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import pl.squad.shop.model.Item;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item, Long>{
+	
+	
+
+}
