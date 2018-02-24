9ff84e295b3294c2a5833e2231e194ada5e90b2d
Michal_Fromlewicz <michal.fromlewicz@gmail.com>
2018-02-24 11:23:39 +0100

    controller packgage created, Class Shop Controller started

---
+++ src/main/java/pl/squad/shop/controller/ShopController.java
@@ -0,0 +1,15 @@
+package pl.squad.shop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ShopController {
+	
+	@GetMapping("/")
+	public String index() {
+		
+		return "index";
+	}
+
+}
